ae8a5aa4b19e8c4d7db3b0cf487a0b93c7117496
kmjak <tymkoron1221@icloud.com>
2025-06-05 17:56:23 +0900

    style: クイズ画面のスタイルを整えた

--- AnimalQuiz/QuizView.swift
+++ AnimalQuiz/QuizView.swift
@@ -9,28 +9,49 @@ import SwiftUI
 
 struct QuizView: View {
     @State var isShowingScoreView: Bool = false
-    
+    let choices = [
+        "ライオン",
+        "ウサインボルト",
+        "チーター",
+        "馬"
+    ]
+
     var body: some View {
         VStack {
             Text("問題番号: 1/5")
-            Text("次のうち、世界で最も速く走る動物はどれですか？")
+                .font(.headline)
+                .padding(10)
+                .background(.originalGreen)
+                .foregroundStyle(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
             
-            Button("ライオン"){
-                isShowingScoreView = true
-            }
-            .fullScreenCover(isPresented: $isShowingScoreView) {
-                ScoreView()
-            }
-            Button("ウサイン・ボルト"){
-                
-            }
-            Button("チーター"){
-                
-            }
-            Button("馬"){
-                
+            Text("次のうち、世界で最も速く走る動物はどれですか？")
+                .font(.title)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(.originalLightGreen)
+                .overlay(RoundedRectangle(cornerRadius: 10)
+                    .stroke(.originalGreen, lineWidth: 5))
+                .frame(maxHeight: .infinity)
+            
+            
+            ForEach(choices, id: \.self){ choice in
+                Button(choice){
+                    isShowingScoreView = true
+                }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .font(.title.bold())
+                .background(.originalSkin)
+                .foregroundStyle(.originalBrown)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .fullScreenCover(isPresented: $isShowingScoreView) {
+                    ScoreView()
+                }
             }
         }
+        .padding()
+        .backgroundImage()
     }
 }
 
